7d77831ff270b543dfba5f62d2da5424bfab4832
ads <michael.adrian.villareal@adamson.edu.ph>
2024-04-04 15:44:29 +0800

    First-Project

--- 1-simple-calculator/calculator.lisp
+++ 1-simple-calculator/calculator.lisp
@@ -1,10 +1,12 @@
 ;;; a simple calculator that accepts only non-decimal integer.
-;;; you can just modified this code in able to experiment and enhance whatever you like, This is just a representation of how to create a simple calculator in a way of defining a function (using conditional,case and assignment such as let)and reading an input using *query-io* 
+;;; you can just modified this code in able to experiment and enhance whatever you like, This is just a representation of how to create a simple calculator in a way of defining a function (using conditional,case and assignment such as let)and reading an input using *query-io*
+;;; Formatting Lisp Code, to have a reference about format: go to https://gigamonkeys.com/book/a-few-format-recipes.
 
 (defun calculate (num1 num2 operator)
   "Perform arithmetic operation based on operator."
   (cond
-    ((eql operator 'a) (+ num1 num2))
+    ;; EQL is a function used for testing whether its arguments are structurally identical. It is similar to EQUAL, but stricter in its comparison.
+    ((eql operator 'a) (+ num1 num2)) 
     ((eql operator 'b) (- num1 num2))
     ((eql operator 'c) (* num1 num2))
     ((eql operator 'd) (/ num1 num2))
@@ -12,10 +14,11 @@
 
 (defun read-number ()
   "Read a number from user input."
-  (format *query-io* "Enter a number: ")
+  (format *query-io* "Enter a number: ") ;; *query-io* is a variable (which you can tell because of the naming convention for special variables) that contains the input streams connected to the terminal. 
   (finish-output *query-io*)
   (read *query-io*))
 
+;;; Assignment using let which is used to create new variable bindings. 
 (defun read-operator ()
   "Read an operator from user input."
   (format *query-io* "Enter an operator:~%a.) Addition~%b.) Subtraction~%c.) Multiplication~%d.) Division~%")
@@ -37,5 +40,5 @@
       (let ((operator (read-operator)))
         (format t "Result: ~a~%" (calculate num1 num2 operator))))))
 
-;; Run the calculator
+;; Sample Usage
 (simple-calculator)
